0540351a963ad80b512e9018d597d0fb245363b2
Alexander Zhukov <alexander.zhukov@parexel.com>
2017-09-21 19:07:31 +0300

    Changes in ArticleFactory.java

--- src/test/java/Articles/ArticleFactory.java
+++ src/test/java/Articles/ArticleFactory.java
@@ -5,17 +5,25 @@ import java.util.ResourceBundle;
 public class ArticleFactory {
 
     static ResourceBundle resourceArticle;
+    Article article;
 
     static {
         resourceArticle = ResourceBundle.getBundle("articles");
     }
 
-    public static Article getValidArticle() {
-        String title = resourceArticle.getString("article.validTitle");
-        String content = resourceArticle.getString("article.validContent");
-        String tag = resourceArticle.getString("article.validTag");
-        String author = resourceArticle.getString("article.validAuthor");
-        return new Article(title,content,tag,author);
+    public static Article getValidArticleAllFields() {
+        Article article = new Article();
+        article.setTitle(resourceArticle.getString("article.validTitle"));
+        article.setContent(resourceArticle.getString("article.validContent"));
+        article.setAuthor(resourceArticle.getString("article.validAuthor"));
+        article.setTag(resourceArticle.getString("article.validTag"));
+        return article;
+    }
+
+    public static Article getArticleTitle() {
+        Article article = new Article();
+        article.setTitle(resourceArticle.getString("article.validTitle"));
+        return article;
     }
 
 }
